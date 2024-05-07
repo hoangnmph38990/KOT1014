a5b6b59bd4c92208baa8f275f8c32c93f8584278
Manhhoang <hoangnmph38990@fpt.edu.vn>
2024-05-07 08:59:14 +0700

    Commit

--- app/src/main/java/com/example/hello_word/lab1.kt
+++ app/src/main/java/com/example/hello_word/lab1.kt
@@ -1,7 +1,7 @@
 package com.example.hello_word
 
 
-fun main(){
+fun main() {
     println("Nguyen Manh Hoang");
     println("Lab 1 - MD18307");
     //
@@ -16,13 +16,15 @@ fun main(){
     val soA = 10
     val soB = 5f
 
-    println("Thuong 2 so $soA & $soB la : " + phepchia(soA,soB));
+    println("Thuong 2 so $soA & $soB la : " + phepchia(soA, soB));
+    bai1()
+    bai2()
 
 }
 
-fun phepchia ( soA : Int, soB :Float) : Float {
+fun phepchia(soA: Int, soB: Float): Float {
 
-    if (soB == 0f){
+    if (soB == 0f) {
         return 0f
     }
 
@@ -31,6 +33,33 @@ fun phepchia ( soA : Int, soB :Float) : Float {
     return thuong
 }
 
+fun bai1() {
+    println("\n");
+    print("Nguyễn Mạnh Hoàng - PH38990\n")
+    println("Quanh năm buôn bán ở mom sông")
+    println("Nuôi đủ năm con với một chồng")
+    println("\tlặn lội thân cờ khi quãng vắng")
+    println("\teo sèo mặt nước buổi đò đồng")
+    println("Một duyên hai nợ âu đành phận")
+    println("Năm nắng mười mưa há chẳng công")
+    println("\tCha mẹ thói đời \"ăn ở bạc\"")
+    println("\tCó chồng hờ hững cũng như không.")
+    println("---------------------------------")
+}
 
+fun bai2() {
+        var a = 0.0;
+        var b = 0.0;
+        println("Nhập a:");
+        var s = readLine();
+        if (s != null) a = s.toDouble();
+        println("Nhập b:");
+        s = readLine();
+        if (s != null) b = s.toDouble();
+        println("Tổng: " + (a + b));
+        println("Hiệu: " + (a - b));
+        println("Tích: " + (a * b));
+        println("Thương: " + (a / b));
+}
 class lab1 {
 }
